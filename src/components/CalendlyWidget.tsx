a685c9b0d2cc625b9919fbf799852f1118f940d4
Héctor Guerra <hectorluengo.xx@gmail.com>
2025-01-26 23:12:37 +0100

    feat:

--- src/components/CalendlyWidget.tsx
+++ src/components/CalendlyWidget.tsx
@@ -6,13 +6,13 @@ declare global {
 }
 
 export default function CalendlyButton({ children, className }: { children: any, className?: string }) {
-  const eventName = "asesoria-gratuita-1-a-1";
+  const eventName = "llamada-1-a-1";
   const backgroundColor = "292929";
   const textColor = "ffffff";
   const primaryColor = "16d378";
   const cookiesBanner = 1;
 
-  const calendlyUrl = `https://calendly.com/hector-guerra/${eventName}?hide_gdpr_banner=${cookiesBanner}&background_color=${backgroundColor}&text_color=${textColor}&primary_color=${primaryColor}`;
+  const calendlyUrl = `https://calendly.com/hectorguerra_io/${eventName}?hide_gdpr_banner=${cookiesBanner}&background_color=${backgroundColor}&text_color=${textColor}&primary_color=${primaryColor}`;
 
   useEffect(() => {
     const link = document.createElement('link');
